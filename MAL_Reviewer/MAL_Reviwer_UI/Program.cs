bad395ab8ff2c7344dc7ccc7ab5b0c31f09d0b6b
Eoussama <oussama_gtacj@live.fr>
2018-10-24 14:55:49 +0100

    Initial files added

---
+++ MAL_Reviewer/MAL_Reviwer_UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MAL_Reviwer_UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MAL_Reviwer_UI.forms.fWelcome());
+        }
+    }
+}
---
+++ MAL_Reviewer/MAL_Reviwer_UI/forms/fWelcome.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MAL_Reviwer_UI.forms
+{
+    partial class fWelcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fWelcome));
+            this.SuspendLayout();
+            // 
+            // fWelcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fWelcome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MAL Reviewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
