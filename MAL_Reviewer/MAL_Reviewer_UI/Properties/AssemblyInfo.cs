14778deb747deb8b884aa40da8d873c805e67e67
Eoussama <oussama_gtacj@live.fr>
2018-11-09 12:02:19 +0000

    Project renamed

--- MAL_Reviewer/MAL_Reviewer_UI/Properties/AssemblyInfo.cs
+++ MAL_Reviewer/MAL_Reviewer_UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MAL_Reviwer_UI")]
+[assembly: AssemblyTitle("MAL_Reviewer_UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MAL_Reviwer_UI")]
+[assembly: AssemblyProduct("MAL_Reviewer_UI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
