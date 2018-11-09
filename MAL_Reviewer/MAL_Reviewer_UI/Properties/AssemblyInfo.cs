66904e80e717f8a5b070785c20f59108be8e0b67
Eoussama <oussama_gtacj@live.fr>
2018-11-09 13:36:44 +0000

    Added desc in the asemblyinfo file.

--- MAL_Reviewer/MAL_Reviewer_UI/Properties/AssemblyInfo.cs
+++ MAL_Reviewer/MAL_Reviewer_UI/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MAL_Reviewer_UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An unofficial Anime and Manga reviewing tool for MyAnimeList.net.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MAL_Reviewer_UI")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
