83382601bc861c64117e9bc074ac3990ac575a75
Eoussama <oussama_gtacj@live.fr>
2018-11-13 17:17:06 +0000

    added custom textbox to the welcome page.

--- MAL_Reviewer/MAL_Reviewer_UI/forms/WelcomeForm.Designer.cs
+++ MAL_Reviewer/MAL_Reviewer_UI/forms/WelcomeForm.Designer.cs
@@ -35,8 +35,6 @@
             this.pUser = new System.Windows.Forms.Panel();
             this.bUser = new System.Windows.Forms.Button();
             this.pReviewLookUp = new System.Windows.Forms.Panel();
-            this.pbSearch = new System.Windows.Forms.PictureBox();
-            this.tbSearch = new System.Windows.Forms.TextBox();
             this.pButtons = new System.Windows.Forms.Panel();
             this.bClear = new System.Windows.Forms.Button();
             this.ilSizeControl = new System.Windows.Forms.ImageList(this.components);
@@ -139,10 +137,10 @@
             this.lTitle = new System.Windows.Forms.Label();
             this.pbLogo = new System.Windows.Forms.PictureBox();
             this.ttExtendedInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.reviewSearchControl = new MAL_Reviewer_UI.user_controls.TextboxControl();
             this.pSide.SuspendLayout();
             this.pUser.SuspendLayout();
             this.pReviewLookUp.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).BeginInit();
             this.pButtons.SuspendLayout();
             this.pMain.SuspendLayout();
             this.pDashboard.SuspendLayout();
@@ -226,33 +224,12 @@
             // pReviewLookUp
             // 
             this.pReviewLookUp.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.pReviewLookUp.Controls.Add(this.pbSearch);
-            this.pReviewLookUp.Controls.Add(this.tbSearch);
+            this.pReviewLookUp.Controls.Add(this.reviewSearchControl);
             this.pReviewLookUp.Dock = System.Windows.Forms.DockStyle.Top;
             this.pReviewLookUp.Location = new System.Drawing.Point(0, 73);
             this.pReviewLookUp.Name = "pReviewLookUp";
             this.pReviewLookUp.Size = new System.Drawing.Size(230, 35);
             this.pReviewLookUp.TabIndex = 2;
-            // 
-            // pbSearch
-            // 
-            this.pbSearch.Image = ((System.Drawing.Image)(resources.GetObject("pbSearch.Image")));
-            this.pbSearch.Location = new System.Drawing.Point(204, 7);
-            this.pbSearch.Name = "pbSearch";
-            this.pbSearch.Size = new System.Drawing.Size(20, 20);
-            this.pbSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbSearch.TabIndex = 1;
-            this.pbSearch.TabStop = false;
-            // 
-            // tbSearch
-            // 
-            this.tbSearch.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.tbSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tbSearch.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbSearch.Location = new System.Drawing.Point(9, 7);
-            this.tbSearch.Name = "tbSearch";
-            this.tbSearch.Size = new System.Drawing.Size(188, 20);
-            this.tbSearch.TabIndex = 0;
             // 
             // pButtons
             // 
@@ -414,7 +391,7 @@
             this.pDashAbout.Location = new System.Drawing.Point(0, 954);
             this.pDashAbout.Margin = new System.Windows.Forms.Padding(0, 19, 0, 0);
             this.pDashAbout.Name = "pDashAbout";
-            this.pDashAbout.Size = new System.Drawing.Size(591, 186);
+            this.pDashAbout.Size = new System.Drawing.Size(591, 181);
             this.pDashAbout.TabIndex = 17;
             // 
             // rtbAbout
@@ -425,7 +402,7 @@
             this.rtbAbout.Name = "rtbAbout";
             this.rtbAbout.ReadOnly = true;
             this.rtbAbout.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.rtbAbout.Size = new System.Drawing.Size(591, 186);
+            this.rtbAbout.Size = new System.Drawing.Size(591, 181);
             this.rtbAbout.TabIndex = 20;
             this.rtbAbout.Text = "Such empty";
             // 
@@ -1552,7 +1529,22 @@
             this.ttExtendedInfo.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
             this.ttExtendedInfo.ToolTipTitle = "Information";
             // 
-            // Welcome
+            // reviewSearchControl
+            // 
+            this.reviewSearchControl.AllowLoad = false;
+            this.reviewSearchControl.AutoSubmit = false;
+            this.reviewSearchControl.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.reviewSearchControl.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.reviewSearchControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reviewSearchControl.Icon = global::MAL_Reviewer_UI.Properties.Resources.icon_search;
+            this.reviewSearchControl.InnerText = "";
+            this.reviewSearchControl.Location = new System.Drawing.Point(0, 0);
+            this.reviewSearchControl.Name = "reviewSearchControl";
+            this.reviewSearchControl.Size = new System.Drawing.Size(230, 35);
+            this.reviewSearchControl.TabIndex = 7;
+            this.reviewSearchControl.ToggleIcon = true;
+            // 
+            // WelcomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -1561,15 +1553,13 @@
             this.Controls.Add(this.pSide);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(1000, 620);
-            this.Name = "Welcome";
+            this.Name = "WelcomeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MAL Reviewer";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.pSide.ResumeLayout(false);
             this.pUser.ResumeLayout(false);
             this.pReviewLookUp.ResumeLayout(false);
-            this.pReviewLookUp.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbSearch)).EndInit();
             this.pButtons.ResumeLayout(false);
             this.pMain.ResumeLayout(false);
             this.pDashboard.ResumeLayout(false);
@@ -1621,8 +1611,6 @@
         private System.Windows.Forms.Button bOpen;
         private System.Windows.Forms.Panel pReviewLookUp;
         private System.Windows.Forms.ImageList ilSizeControl;
-        private System.Windows.Forms.PictureBox pbSearch;
-        private System.Windows.Forms.TextBox tbSearch;
         private System.Windows.Forms.Button bSettings;
         private System.Windows.Forms.Panel pUser;
         private System.Windows.Forms.Button bUser;
@@ -1719,5 +1707,6 @@
         private System.Windows.Forms.Button bMALMangalist;
         private System.Windows.Forms.Button bMALProfile;
         private System.Windows.Forms.Button bMALFriends;
+        private user_controls.TextboxControl reviewSearchControl;
     }
 }
