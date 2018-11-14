90401aeea3ca4009429346625178c8362ce40550
Eoussama <oussama_gtacj@live.fr>
2018-11-14 16:22:23 +0000

    buttons tooltips.

---
+++ MAL_Reviewer/MAL_Reviewer_UI/forms/sub_forms/SettingsTemplateForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MAL_Reviewer_UI.forms.sub_forms
+{
+    public partial class SettingsTemplateForm : Form
+    {
+        public SettingsTemplateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
