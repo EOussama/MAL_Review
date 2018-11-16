310bd2410a08065d82b15291796797a1e86c0cc4
Eoussama <oussama_gtacj@live.fr>
2018-11-16 18:07:36 +0000

    Added basic file checking

--- MAL_Reviewer/MAL_Reviewer_Review/Settings.cs
+++ MAL_Reviewer/MAL_Reviewer_Review/Settings.cs
@@ -1,4 +1,7 @@
-﻿using MAL_Reviewer_Core.controllers;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using MAL_Reviewer_Core.controllers;
 
 namespace MAL_Reviewer_Core
 {
@@ -7,13 +10,54 @@ namespace MAL_Reviewer_Core
     /// </summary>
     public static class Settings
     {
+        /// The path where the storage folder is(should be) located.
+        private static readonly string STORAGE_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        /// <summary>
+        ///  The storage folder and file.
+        /// </summary>
+        private const string
+            STORAGE_FOLDER = "MAL_Reviewer",
+            STORAGE_FILE = "settings.bat";
+
         /// <summary>
         /// Initializing the settings.
         /// </summary>
         public static void Init()
         {
-            // Seeding default settings.
-            SeedSettings();
+            // Check if the storage folder exists.
+            if (DoesStorageFolderExist())
+            {
+                // Check if storage file exists.
+                if (!DoesStorageFileExist())
+                {
+                    // Seeding default settings.
+                    SeedSettings();
+
+                    // Serializing the data.
+                }
+                else
+                {
+                    // Load
+                }
+            }
+            else
+            {
+                // Creating the storage folder.
+                Directory.CreateDirectory(Path.Combine(STORAGE_PATH, STORAGE_FOLDER));
+
+                // Seeding default settings.
+                SeedSettings();
+
+                // Serializing the data.
+                using (FileStream fileStream = new FileStream(Path.Combine(STORAGE_PATH, STORAGE_FILE), FileMode.Create))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                    binaryFormatter.Serialize(fileStream, "");
+                    fileStream.Close();
+                }
+            }
         }
 
         /// <summary>
@@ -40,6 +84,24 @@ namespace MAL_Reviewer_Core
         {
             // Seeding review template settings.
             ReviewTemplatesController.SeedSettings();
+        }
+
+        /// <summary>
+        /// Check if the folder where the data resides exists.
+        /// </summary>
+        /// <returns></returns>
+        public static bool DoesStorageFolderExist()
+        {
+            return Directory.Exists(Path.Combine(STORAGE_PATH, STORAGE_FOLDER));
+        }
+
+        /// <summary>
+        /// Check if the file where the data resides exists.
+        /// </summary>
+        /// <returns></returns>
+        public static bool DoesStorageFileExist()
+        {
+            return File.Exists(Path.Combine(STORAGE_PATH, STORAGE_FILE));
         }
     }
 }
